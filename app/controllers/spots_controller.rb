4110eaf41e55c9df11576cd23380eba63e822b47
okazuo <okazuokakazu@gmail.com>
2021-10-28 13:11:26 +0900

    ルートパスをトップページに変更

---
+++ app/controllers/spots_controller.rb
@@ -0,0 +1,7 @@
+class SpotsController < ApplicationController
+
+def index
+end
+
+
+end
---
+++ db/migrate/20211028034621_create_spots.rb
@@ -0,0 +1,18 @@
+class CreateSpots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spots do |t|
+      t.integer :price
+      t.string  :estate_agent
+      t.integer :size
+      t.string :address
+      t.integer :water_id
+      t.integer :officialmap_id
+      t.integer :transcript_id
+      t.text :explanation
+      t.string :pdf
+      t.references :user, null: false, foregign_key: true
+
+      t.timestamps
+    end
+  end
+end
