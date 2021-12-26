b9571a74621d73b82e2eec71009cdb22364d2567
okazuo <okazuokakazu@gmail.com>
2021-12-26 17:21:07 +0900

    アソシエーション、マイグレーション

--- db/migrate/20211226070111_create_orders.rb
+++ db/migrate/20211226070111_create_orders.rb
@@ -1,7 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.string :customer
+      t.string :not_tradeable
+      t.references :user, foregign_key: true
+      t.references :spot, foregign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_022620) do
+ActiveRecord::Schema.define(version: 2021_12_26_070111) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2021_11_04_022620) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "customer"
+    t.string "not_tradeable"
+    t.bigint "user_id"
+    t.bigint "spot_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["spot_id"], name: "index_orders_on_spot_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
