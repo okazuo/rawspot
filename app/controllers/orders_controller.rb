3b659f463f166aa5a40d8f6ac9baf62644d324cc
okazuo <okazuokakazu@gmail.com>
2021-12-27 19:00:36 +0900

    バリデーション、createのコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :get_recode, only: [:index, :create]
+
   def index
-    @spot= Spot.find(params[:spot_id])
-    @order= Order.new
+    @order = Order.new
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+    render 'index'
+    end
+  end
+
+  private
+
+  def get_recode
+    @spot = Spot.find(params[:spot_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:customer, :not_tradeable,).merge(user_id: current_user.id, spot_id: @spot.id)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_one :spot
+
+  validate :required_either_customer_or_not_tradeable
+
+  private
+
+  def required_either_customer_or_not_tradeable
+    # customerかnot_tradeableのどちらかの値があれば true 、共にあればfalse
+    if customer.present? ^ not_tradeable.present?
+      return  
+    end
+    errors.add(:base, 'お客様名を入力してください')
+  end
 end
