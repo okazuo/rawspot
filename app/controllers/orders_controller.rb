6aa2709e68162ab98f255650ed6e15145e0c17f8
okazuo <okazuokakazu@gmail.com>
2021-12-27 16:30:46 +0900

    購入のビュー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
   def index
+    @spot= Spot.find(params[:spot_id])
+    @order= Order.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'spots#index'
   resources :spots do
-    resources :order , only: [:index, :create]
     collection do
       get 'search'
     end
+    resources :orders , only: [:index, :create]
   end
 end
