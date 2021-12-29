5ac467db7b6a3aece3212a2675210f26a2c92db2
okazuo <okazuokakazu@gmail.com>
2021-12-29 16:19:28 +0900

    ボタンと遷移先への表示非表示とリダイレクト

--- app/controllers/closes_controller.rb
+++ app/controllers/closes_controller.rb
@@ -1,18 +1,20 @@
 class ClosesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :get_recode, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @close = Close.new
   end
 
-def create
-  @close = Close.new(close_params)
-  @close.not_available = 1
-  @close.save
-  redirect_to root_path
-end
+  def create
+    @close = Close.new(close_params)
+    @close.not_available = 1
+    @close.save
+    redirect_to root_path
+  end
 
-
+  
   private
 
   def get_recode
@@ -23,4 +25,12 @@ end
     params.require(:close).permit(:not_available).merge(user_id: current_user.id, spot_id: @spot.id)
   end
 
+  def move_to_index
+    unless current_user.id == @spot.user.id
+      redirect_to root_path
+    end 
+    if @spot.order.present? || @spot.close.present?
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :get_recode, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order = Order.new
@@ -15,6 +16,7 @@ class OrdersController < ApplicationController
     end
   end
 
+  
   private
 
   def get_recode
@@ -23,5 +25,14 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:customer).merge(user_id: current_user.id, spot_id: @spot.id)
+  end
+
+  def move_to_index
+    unless current_user.id == @spot.user.id
+      redirect_to root_path
+    end 
+    if @spot.order.present? || @spot.close.present?
+      redirect_to root_path
+    end
   end
 end
