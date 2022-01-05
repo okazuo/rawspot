671cc7895ce1a83b99610d941dd1f5c13e8276aa
okazuo <okazuokakazu@gmail.com>
2022-01-05 17:41:03 +0900

    closeモデル単体テストコード

--- spec/factories/closes.rb
+++ spec/factories/closes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :close do
-    
+    not_available{1}
+    association :user
+    association :spot
   end
 end
--- spec/models/close_spec.rb
+++ spec/models/close_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Close, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @close = FactoryBot.build(:close)
+  end
+
+  describe '取り扱い不可機能' do
+    context '取り扱い不可にできる場合' do
+      it 'not_availableが存在すれば登録できる' do
+        expect(@close).to be_valid
+      end
+    end
+
+    context '取り扱い不可にできない場合' do
+      it 'not_availableが存在しなければ登録できない' do
+        @close.not_available = ''
+        @close.valid?
+        expect(@close.errors.full_messages).to include('Not availableを入力してください')
+      end
+      it 'user_idが存在しなければ契約できない' do
+        @close.user = nil
+        @close.valid?
+        expect(@close.errors.full_messages).to include('Userを入力してください')
+      end
+      it 'spot_idが存在しなければ契約できない' do
+        @close.spot = nil
+        @close.valid?
+        expect(@close.errors.full_messages).to include('Spotを入力してください')
+      end
+    end
+  end
 end
