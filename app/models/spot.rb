0caddbef7a9f722a0b6653572eef1158f649d7af
okazuo <okazuokakazu@gmail.com>
2022-01-16 18:14:18 +0900

    アソシエーションとバリデーション

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,11 +2,12 @@ class Spot < ApplicationRecord
   with_options presence: true do
     validates :estate_agent, length: { maximum: 50 }
     validates :address, length: { maximum: 50 }
-    
+
   end
 
   validates :size, numericality: true, allow_blank: true, length: { maximum: 4 }
   validates :price, numericality: true, allow_blank: true, length: { maximum: 5 }
+  validates :images, length: {maximum: 4, massage: 'は４枚以下にしてください'}
 
   with_options numericality: { other_than: 1, message: "を選択してください" } do
     validates :water_id
@@ -18,7 +19,7 @@ class Spot < ApplicationRecord
   validates :explanation, length: { maximum: 1000 }
 
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_one :order
   has_one :close
   has_many :comments
