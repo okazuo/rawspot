3af02859fbde37ee3282fb977b98fc7a41ba5c0e
okazuo <okazuokakazu@gmail.com>
2022-02-06 23:04:09 +0900

    投稿中の待機画面を作成

---
+++ app/javascript/screenlook.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const spotForm = document.getElementById('new_spot');
+  const wait_btn = document.getElementById('wait_btn')
+  if (!spotForm) return null;
+  
+  wait_btn.addEventListener('click',function(){
+    console.log('aaa')
+    const element = document.createElement('div')
+    element.style.height = '100%';
+    element.style.position = 'fixed';
+    element.style.left = '0px';
+    element.style.top = '0px';
+    element.style.width = '100%';
+    element.style.zIndex = '999';
+    element.style.opacity = '0.5';
+    element.style.backgroundColor = 'black';
+    const objBody = document.getElementsByTagName("body").item(0);
+    objBody.appendChild(element);
+
+    const logo =document.getElementById('animation')
+    logo.style.display='block'
+    const marker =document.getElementById('animation_marker')
+    marker.style.display='block'
+    const animessage =document.getElementById('animation_message')
+    animessage.style.display='block'
+  })
+})
