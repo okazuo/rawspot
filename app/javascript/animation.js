0787ce292e85a0e004567c992615217a2146821a
okazuo <okazuokakazu@gmail.com>
2022-02-07 21:29:24 +0900

    footerロゴのアニメーション

--- app/javascript/animation.js
+++ app/javascript/animation.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', function(){
   const animation_switch = document.getElementById('animation_switch');
   
   animation_switch.addEventListener('mouseover',function(){
-    const logo =document.getElementById('animation')
-    const marker =document.getElementById('animation_marker')
+    const logo =document.getElementById('gradation_animation')
+    const marker =document.getElementById('gradation_animation_marker')
     const timeout_id = setTimeout(() => {
       logo.style.display='block'
-      logo.style.opacity = '0.7';
       marker.style.display='block'
-      marker.style.opacity = '0.7';
-    }, 2000);
+    }, 3000);
     
     animation_switch.addEventListener('mouseout',function(){
       clearTimeout(timeout_id)
